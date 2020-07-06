d41d3bac880dc32f973189fc546a63760f727e6f
techellipticaeducation <techellipticaeducation@gmail.com>
2020-07-07 01:47:26 +0530

    session4

---
+++ src/test/Human.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Human {
+	public static void main(String[] args) {
+			System.out.println("test");
+	}	
+}
+
+class A{
+	
+}
+
+class B{
+	
+}
+
+class C{
+	
+}
+
---
+++ src/test1/A.java
@@ -0,0 +1,18 @@
+package test1;
+
+public class A {
+	public static void main(String[] args) {
+		System.out.println("A");
+	}
+}
+
+class B{
+	
+}
+
+class C{
+	public static void main(String[] args) {
+		System.out.println("C");
+	}
+}
+
